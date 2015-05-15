e22af52b0086700869446fdf62c3719907a9d75a
Stuart Lee <lee.s@wehi.edu.au>
2015-05-15 22:55:51 +1000

    New MSE method, automatic model selection implemented

--- R/binomial_mixture_utility.R
+++ R/binomial_mixture_utility.R
@@ -27,13 +27,62 @@ aic <- function(mixture) {
 #' Mean Square Error for binomial mixture model
 #'
 #' @param mixture estimated mixture model
-#' @param pi     true mixture weights
-mseMM <- function(mixture, pi) {
+#' @param theta     true mixture-model parameters of form (pi_1,..,pi_k, mu_1,...mu_k)
+#' @export
+mseMM <- function(mixture, theta) {
   # potential matching problem
   # which we avoid by sorting the components
+  stopifnot(length(theta) == 2*mixture$k)
+  stopifnot(sum(theta[1:mixture$k]) == 1)
+
+  k.index <- 1:mixture$k
   pi.hat <- sort(mixture$pi)
-  pi <- sort(pi)
-  mean((pi - pi.hat)^2)
+  mu.hat <- sort(mixture$mu)
+  pi <- sort(theta[1:k])
+  mu <- sort(theta[(k+1):(2*k)])
+  mse <- list(pi.mse = mean((pi - pi.hat)^2),
+              mu.mse = mean((mu - mu.hat)^2),
+              all.mse = mean((c(pi,mu) - c(pi.hat,mu.hat))^2))
+  return(mse)
+}
+
+#' Use an information criterion to perform model selection
+#' for number of mixture components
+#'
+#' @param x vector of observed read counts
+#' @param N coverage
+#' @param k maximum number of components to estimate
+#' @param method string with information criterion either 'bic' or 'aic'
+autoSelect <- function(x, N, k, method) {
+  if(k < 1 | k > 5) {
+    stop("Maximum number of components must be ")
+  }
+  if(!(method %in% c("aic", "bic"))) {
+    stop("Method must either be aic or bic")
+  }
+  selector <- match.fun(method)
+
+  # initalise
+  crits <- c()
+  models <- list()
+  for (i in 1:k) {
+    model <- binommixEM(x, N, i)
+    ic <- selector(model)
+    models[[i]] <- list(model = model)
+    crits[i] <- ic
+  }
+
+  min.ic <- which.min(crits)
+
+  print(paste("The ", method, "for each choice k:"))
+  print(paste(1:k, "\t", crits))
+
+  results <- data.frame(component = 1:k, values = crits)
+  best.model <- model[[min.ic]]
+
+  return(list(method = method,
+              info.crit = results,
+              model = best.model))
 }
 
 #' CDF for binomial mixture model
