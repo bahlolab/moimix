78449c12f098cbefc4a9e3510b8162da76e1b307
Stuart Lee <lee.s@wehi.edu.au>
2016-01-29 14:59:07 +1100

    added functions to extract major allele calls, estimate BAF quickly

--- R/seqarray_process.R
+++ R/seqarray_process.R
@@ -8,21 +8,23 @@ processGATK <- function(gdsfile, ref.allele) {
     read_counts <- SeqArray::seqGetData(gdsfile, "annotation/format/AD")$data
     # for each variant the AD tag contains the ref and alt counts
     nsnps <- ncol(read_counts)
-    ref_index <- seq(1, nsnps*2, by = 2) # odd index are ref counts
-    alt_index <- seq(2, nsnps*2, by = 2) # even index are alt counts
-    
-    if (ref.allele == 0L) {
-        return(read_counts[, ref_index])
-    }
-    
-    if (ref.allele == 1L) {
-        return(read_counts[, alt_index])
-    }
+    ref_index <- seq(1, nsnps, by = 2) # odd index are ref counts
+    alt_index <- seq(2, nsnps, by = 2) # even index are alt counts
     
     if (is.null(ref.allele)) {
         return(list(ref = read_counts[, ref_index],
                     alt = read_counts[, alt_index]))
     }
+    
+    else if (ref.allele == 0L) {
+        return(read_counts[, ref_index])
+    }
+    
+    else if (ref.allele == 1L) {
+        return(read_counts[, alt_index])
+    }
+    
+
     
 }
 
@@ -44,22 +46,21 @@ processVarscan <- function(gdsfile, ref.allele) {
 #' Return allele counts from a genofile based on read counts
 #' 
 #' @param gdsfile a \code{\link[SeqArray]{SeqVarGDSClass}} object
-#' @param vcf.caller a character string with variant caller name, currently 
-#' supports 'gatk_ug', 'gatk_hc', 'varscan'
 #' @param ref.allele \code{NULL} a single numeric value.
 #' @details If \code{ref.allele = NULL}, the function returns a list
 #' of matrices corresponding to the read counts of the reference and alternate
 #' alleles respectively, while if \code{ref.allele = 0L} or \code{ref.allele = 1L}
 #' the function returns a  matrix corresponding the reference or alternate
-#' read counts.
-#' @importClassesFrom SeqArray SeqVarGDSClass
+#' read counts. Currently, the variant formats from varscan and GATK
+#' are supported.
+#' @importFrom SeqArray seqSummary
 #' @export 
-alleleCounts <- function(gdsfile, vcf.caller, ref.allele = NULL) {
+alleleCounts <- function(gdsfile, ref.allele = NULL) {
     # I/O checks
-    stopifnot(inherits(genofile, "SeqVarGDSClass"))
+    stopifnot(inherits(gdsfile, "SeqVarGDSClass"))
     stopifnot(is.null(ref.allele) | ref.allele %in% c(0L,1L))
     
-    vars <- seqSummary(gdsfile, "annotation/format", check="none", verbose=FALSE)$ID
+    vars <- seqSummary(gdsfile, check="none", verbose=FALSE)$format$var.name
     
     # GATK will have just AD but no RD
     if("AD" %in% vars && !("RD" %in% vars))  {
@@ -76,39 +77,79 @@ alleleCounts <- function(gdsfile, vcf.caller, ref.allele = NULL) {
 
 }
 
-#' Get major allele for isolates
+#' Get major allele for isolates from BAF matrix
 #' 
 #' @param gdsfile a \code{\link[SeqArray]{SeqVarGDSClass}} object
+#' @param get.nucleotides FALSE logical indicating whether to replace allele
+#' with nucleotide bases
 #' 
 #' @description Extract major alleles from a GDS file
-#' using the Reference allele frequency (RAF) estimated from 
+#' using the non-reference allele frequency (NRAF) estimated from 
 #' read counts directly. In the case of RAF = 0.5 we select the
-#' major allele randomly from Bernoulli RV. 
-callMajor <- function(gdsfile) {
+#' major allele randomly from Bernoulli distribution.
+#' @return a binary matrix containing reference and alternate alleles
+#' or if get.nucleotides a character matrix
+#' @importFrom SeqArray ref alt
+#' @export
+callMajor <- function(gdsfile, get.nucleotides = FALSE) {
     stopifnot(inherits(gdsfile, "SeqVarGDSClass"))
-    # estimate RAF matrix
-    raf_matrix <- 1 - getBAF(gdsfile)
-    gt_matrix <- matrix(NA, nrow = nrow(raf_matrix), ncol = ncol(raf_matrix))
+    # estimate BAF matrix
+    baf <- getBAF(gdsfile)
+    gt_matrix <- baf
+    # if baf is less than 0.5, we'll assign reference allele
+    gt_matrix[baf < 0.5] <- 0
+    # if baf is greater than 0.5 we'll assign alternate allele
+    gt_matrix[baf > 0.5] <- 1
+    # if baf is between then we'll assign randomly
+    # first need to find bafs == 0.5 and not missing
+    index <- baf == 0.5 & !is.na(baf)
+    gt_matrix[index] <- rbinom(n = length(gt_matrix[index]), 
+                               size = 1, 
+                               prob = 0.5)
     
-    gt_matrix
+    if (get.nucleotides) {
+        # generate character vectors containing nucleotides
+        ref.alleles <- as.character(ref(gdsfile))
+        alt.alleles <- sapply(1:ncol(gt_matrix), 
+                              function(i) as.character(alt(variants)[[i]]))
+        # pre-assign matrix to fill
+        nt_matrix <- matrix(NA, nrow = nrow(gt_matrix), ncol = ncol(gt_matrix))
+        # array indexes for bases, second column will contain
+        # index for character vector assigned above
+        ref.indices <- which(gt_matrix == 0, arr.ind = TRUE)
+        alt.indices <- which(gt_matrix == 1, arr.ind = TRUE)
+        miss.indices <- which(is.na(gt_matrix), arr.ind = TRUE)
+        # fill matrix
+        nt_matrix[ref.indices] <- ref.alleles[ref.indices[,2]]
+        nt_matrix[alt.indices] <- alt.alleles[alt.indices[,2]]
+        nt_matrix[miss.indices] <- "N"
+        return(nt_matrix)
+    }
     
+    return(gt_matrix)
+
 }
 
 #' Compute B-allele frequency spectrum
 #'
 #' @param gdsfile a \code{\link[SeqArray]{SeqVarGDSClass}} object
-#' @import SeqArray
+#' @importFrom  SeqArray seqSummary seqApply
+#' @export
 getBAF <- function(gdsfile) {
     stopifnot(inherits(gdsfile, "SeqVarGDSClass"))
     # estimate NRAF matrix, currently on GATK vcf file support
-    vars <- seqSummary(gdsfile, "annotation/format", check="none", verbose=FALSE)$ID
+    vars <- seqSummary(gdsfile, check="none", verbose=FALSE)$format$var.name
     if(!("AD" %in% vars)) {
         stop("Must have annotaion/format/AD tag to compute B-allele frequencies")
     }
     
-    seqApply(gdsfile, "annotation/format/AD",
+    # compute BAF for each sample 
+    nrf <- seqApply(gdsfile, "annotation/format/AD",
              function(x) x[,2] / rowSums(x),
              margin = "by.variant",
              as.is = "list")
+    # convert list to matrix
+    baf <- matrix(unlist(nrf), ncol = length(nrf))
     
+    baf
 }
