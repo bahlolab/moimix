a1f360f0ba778bfb055c63f163df85d2028aad0f
Stuart Lee <lee.s@wehi.edu.au>
2016-02-01 12:55:16 +1100

    added heterozygoisty methods and method for estimating Fws

--- R/seqarray_process.R
+++ R/seqarray_process.R
@@ -153,3 +153,91 @@ getBAF <- function(gdsfile) {
     
     baf
 }
+
+#' Compute minor allele frequency 
+#' 
+#' 
+#' @note Currently only supports gds files obtained from GATK callers
+#' @param gdsfile a \code{\link[SeqArray]{SeqVarGDSClass}} object
+#' @importFrom SeqArray seqApply seqSummary
+#' @export
+getMAF <- function(gdsfile) {
+    stopifnot(inherits(gdsfile, "SeqVarGDSClass"))
+    vars <- seqSummary(gdsfile, check="none", verbose=FALSE)$format$var.name
+    if(!("AD" %in% vars)) {
+        stop("Must have annotaion/format/AD tag to compute minor allele frequencies")
+    }
+    
+    maf <- function(x) {
+
+        depth <- sum(x, na.rm = TRUE)
+        # frequency of ref and alt reads
+        coverage <- colSums(x, na.rm = TRUE)
+        min(coverage) / depth
+    }
+    res <- seqApply(gdsfile, "annotation/format/AD",
+                function(x) maf(x),
+                margin = "by.variant",
+                as.is = "list")
+    unlist(res)
+}
+
+#' Compute heterozygosity by sample 
+#' 
+#' 
+getHeterozygosityBySample <- function(gdsfile) {
+    stopifnot(inherits(gdsfile, "SeqVarGDSClass"))
+    # estimate NRAF matrix, currently on GATK vcf file support
+    vars <- seqSummary(gdsfile, check="none", verbose=FALSE)$format$var.name
+    if(!("AD" %in% vars)) {
+        stop("Must have annotaion/format/AD tag to compute B-allele frequencies")
+    }
+    
+    # function for computing heterozygosity at variant site
+    heterozygosity <- function(x) {
+        depth <- rowSums(x)
+        
+        p <- x[,1] / depth
+        q <- x[,2] /depth
+        1 - (p^2 + q^2)
+        
+    }
+    
+    het <- seqApply(gdsfile, "annotation/format/AD", heterozygosity, 
+             margin = "by.variant", as.is = "list")
+    
+    het <- matrix(unlist(het), ncol = length(het))
+    
+    het
+}
+
+#' Compute population level heterozygosity
+getHeterozygosity <- function(gdsfile) {
+    # first compute MAF 
+    maf <- getMAF(gdsfile)
+    1 - (maf^2 + (1-maf)^2)
+    
+}
+
+#' Compute Fws statistic
+#' 
+#' @details 
+#' @references 
+getFws <- function(gdsfile) {
+    # compute heterozygosity at sample level
+    sample.het <- getHeterozygosityBySample(gdsfile)
+    # compute heterozygosity at population level
+    population.het <- getHeterozygosity(gdsfile)
+    # create MAF bins, 0..0.05, 0.05..0.1,...,0.45..0.5 
+    maf.bins <- findInterval(getMAF(gdsfile), seq(0, 0.5, length.out = 11))
+    # find mean population heterozygoisty values
+    mu.population.het <- tapply(population.het, bins, mean)
+    
+    # find mean sample heterozygosity values
+    mu.sample.het <- apply(sample.het, 1, 
+                           function(x) tapply(x, bins, mean, na.rm = TRUE))
+    
+    fws <- apply(mu.sample.het, 2, function(x) 1 -lm(x ~ mu.population.het -1)$coeff)
+                             
+    fws
+}
