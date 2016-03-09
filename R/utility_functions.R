649fa4e4230304abf9b521b223d7069648afe69a
Stuart Lee <lee.s@wehi.edu.au>
2016-03-09 14:55:35 +1100

    modifying namespace exports

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -28,6 +28,20 @@
 #' @source \url{http://www.malariagen.net/data}
 NULL
 
+#' Plasmodium falciparum SNV data from Gambia.
+#' 
+#' Variant calls of polymorphic variants from 65 P.falciparum isolates in The Gambia,
+#' from Amambua-Ngwa A, Tetteh KK et al., 2012 \url{http://www.ncbi.nlm.nih.gov/pubmed/23133397}.
+#' The data was extracted from VCF files and converted to GDS format from Malaria Genomics Consortium Pf3k version 4. See the README file
+#' at \url{ftp://ngs.sanger.ac.uk/production/pf3k/release_4/} for details. The GDS object was obtained
+#' by filtering the MalariaGen VCF files and keeping only biallelic SNPs that were polymorphic in The Gambia
+#' and passed all filters.  
+#' 
+#' @name gambian_isolates
+#' @docType data
+#' @format A \code{\link[SeqArray]{SeqVarGDSClass}} object.
+#' @source \url{ftp://ngs.sanger.ac.uk/production/pf3k/release_4/}
+
 #' @useDynLib moimix
 #' @importFrom Rcpp sourceCpp
 NULL
