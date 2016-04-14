b767575555dc91073f3853c38e7b43459f3416bd
Stuart Lee <lee.s@wehi.edu.au>
2016-04-14 15:59:11 +1000

    updated man pages

--- R/haplotype_models.R
+++
@@ -1,7 +0,0 @@
-# haplotype_models.R
-# Author: Stuart Lee
-# Description: Implementation of EM models from Li et al. 2007
-# method 1 - fixed number of clones
-haploEM <- function(genotype_matrix, clone.number) {
-    
-}
